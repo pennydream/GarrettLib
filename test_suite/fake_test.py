4b5de0d41de221eb0b940640cc052d27ef70bd71
Brendan <brendan.garrett312@gmail.com>
2017-11-11 16:24:47 -0500

    Further pytest learning

--- test_suite/fake_test.py
+++ test_suite/fake_test.py
@@ -1,5 +1,13 @@
-def some_method():
-    return 1
+import pytest
+
+def some_method(x):
+    if not isinstance(x,int):
+        raise TypeError("Please provide an integer argument")
+    return x 
 
 def test_some_method():
-    assert some_method() == 1
+    assert some_method(1) == 1
+
+def test_some_method_raises_typeerror():
+    with pytest.raises(TypeError):
+        some_method("Junk");
--- test_suite/pytest_test.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-def any_method():
-    return 0
-
-def test_any_method():
-    assert any_method() == 0
