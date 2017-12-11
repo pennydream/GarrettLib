bf139e7dbdb3b0ceac0ef5efc4daad52662fa9b9
Brendan <brendan.garrett312@gmail.com>
2017-12-11 00:03:43 -0500

    Finish code TrainTestSplit

--- garrett_lib/TrainTestSplit.py
+++ garrett_lib/TrainTestSplit.py
@@ -1,20 +1,30 @@
 import numpy as np
 import pandas as pd
+import random
 
 class TrainTestSplit():
     
     def __init__(self):pass
     
     def split(self, X, y):
-        n = len(X) 
+
+        if isinstance(X, pd.DataFrame) and isinstance(y, pd.DataFrame):
+            pass
+        else:
+            raise TypeError("Model data is of type Pandas DataFrame.")
+        
+        n = X.shape[0]
         num_train = int(n*0.666)
         num_test = n - num_train
 
-        index = np.random.randint(0,n,num_train)
-        test_index = X.index[map(lambda x: True if x in index else False, X.index.astype(np.ndarray))]
+        #index = np.random.randint(0,n,num_train)
+        train_index = random.sample(X.index, num_train)
+
+        print train_index
+        test_index = X.index[map(lambda x: False if x in train_index else True, X.index.astype(np.ndarray))]
         
-        train_x = X.iloc[index]
-        train_y = y.iloc[index]
+        train_x = X.iloc[train_index]
+        train_y = y.iloc[train_index]
         test_x = X.iloc[test_index]
         test_y = y.iloc[test_index]
         return train_x, train_y, test_x, test_y
--- test_suite/TrainTestSplit_test.py
+++ test_suite/TrainTestSplit_test.py
@@ -10,23 +10,32 @@ def test_TrainTestSplit_init():
     """
     Given a pandas dataframe, test the creation of a model class.
     """
-    some_true = pd.Series([
-         0, 1, 1, 0 
+    X = pd.DataFrame([
+         [1,2],
+         [2,4],
+         [3,4],
+         [2,3]
          ])
 
-    some_test = pd.Series([ 
+    y = pd.DataFrame([ 
          0, 1, 1, 1
          ])
 
-    m = TrainTestSplit(some_true, some_test)
-
-    assert m.getAccuracy("classification") == 0.75 
+    train_x, train_y, test_x, test_y = TrainTestSplit().split(X,y)
+    
+    assert X.shape[0] == (train_x.shape[0] + test_x.shape[0])
 
 def test_TrainTestSplit_dtype():
     """
     Test that the initialization of a TrainTestSplit class throws a type error for 
     things that are not pandas dataframes
     """
+    X = "Junkie data type" 
+
+    y = pd.Series([ 
+         0, 1, 1, 1
+         ])
+
     some_true = "A wrong data type of type string" 
     some_test = 5 #another wrong data type
     with pytest.raises(TypeError):
