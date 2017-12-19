e5ac9d27ce5afa1c0887107dc68846452f94b711
Brendan <brendan.garrett312@gmail.com>
2017-12-19 14:17:58 -0500

    Update logistic regression to pass tests

--- garrett_lib/LogisticRegression.py
+++ garrett_lib/LogisticRegression.py
@@ -7,63 +7,34 @@ import numpy as np
 class LogisticRegression(Classification):
 
 
-    def train(self, labels, num_iters = 100):
-        self.labels = labels
-
-        train_x = self.data
-        train_y = self.labels
-
-        train_x = train_x.as_matrix()
-        train_y = train_y.as_matrix()
-
-        # Add a constant to the training data
+    def train(self, y, num_iters=150):
+        train_x = self.data.as_matrix()
+        train_y = y.as_matrix()[:,0]
         train_x = np.c_[train_x, np.ones(train_x.shape[0])]
-        index = np.arange(train_x.shape[0])
-
-        # Initialize w with random values
         w = np.array(map(lambda x: 1 if x>0 else -1, np.random.randn(train_x.shape[1])))
-
-        # Get the predictions for the initialized w
-        pred_y_inner = np.array(map(lambda x: 1 if np.dot(w,x)>0 else -1, train_x))
-        pocket = 0
-
+        alpha = 0.01
+        pocket = -1
+    
         for i in range(0, num_iters):
-            # Get accuracy for each iteration
-            ca = ComputeAccuracy(pd.DataFrame(train_y)[0], pd.DataFrame(pred_y_inner)[0])
-            acc = ca.getAccuracy("classification")
-            print acc
-            #acc = compute_accuracy(train_y, pred_y_inner)
+            pred_num_y = map(lambda x: 1/(1+np.exp(-np.dot(w,x))), train_x)
+            pred_y = np.array(map(lambda x: 1 if x>0 else -1, pred_num_y))        
+            size = train_y.shape[0]
+            correct = (train_y == pred_y).sum()
+            acc = (correct.astype(np.float) / size) * 100
+            w = w-np.dot(alpha*(pred_y - train_y), train_x)
 
-            # Check if accuracy is better than previous one. If so, save it
-            if acc > pocket:
-                pocket = acc
+            if(acc > pocket):
                 pocket_w = w
-
-            # Check if accuracy on training data is meant. If so, exit.
-            if acc >= 1.0:
-                self.w = w
-                return True;
-
-            # Choose a random misclassified point.
-            random_sample_index = np.random.choice(index[pred_y_inner != train_y[:,0]])
-
-            # Update the weights using w <- w + xy
-            w = w + 0.01*(train_x[random_sample_index]*train_y[random_sample_index])
-            # Update the prediction for this iteration
-            pred_y_inner = np.array(map(lambda x: 1 if np.dot(w,x)>0 else -1, train_x))
-
-        # Return the saved weights with the highest accuracy
+                pocket = acc
         self.w = pocket_w
-
         return True
 
-    def test(self, test_x):
-        test_index = test_x.index
-        test_x = test_x.as_matrix()
+    def test(self, new_data):
+        w = self.w
+        test_x = new_data.as_matrix()
         test_x = np.c_[test_x, np.ones(test_x.shape[0])]
-        ret = pd.Series(np.array(map(lambda x: 1 if np.dot(self.w,x)>0 else -1, test_x)))
-        ret.index = test_index
-        return ret
+        temp = np.array(map(lambda x: 1/(1+np.exp(-np.dot(w,x))), test_x))
+        return pd.Series(map(lambda x: 1 if x>0 else -1, temp))
 
     """
     @abstractmethod
